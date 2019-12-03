ac6a7d94f0a9a6e7798bc4a73627675cce2a5db5
Emmanuel <emmanuel.stroem@gmail.com>
2019-12-04 03:27:14 +0800

    Remove STATIC_ROOT from STATICFILES_DIR

--- stackanswers/settings.py
+++ stackanswers/settings.py
@@ -138,7 +138,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, 'search/static/'),
     os.path.join(BASE_DIR, 'web/static/'),
 ]
