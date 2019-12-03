7069399174766f23a0a84f4859f43bd02f0d3576
Emmanuel <emmanuel.stroem@gmail.com>
2019-12-04 03:44:27 +0800

    Set different STATIC_ROOT path

--- stackanswers/settings.py
+++ stackanswers/settings.py
@@ -133,11 +133,19 @@ DEBUG = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+#STATIC_ROOT = (os.path.join(SITE_ROOT, 'static_files/'))
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = (os.path.join(SITE_ROOT, 'static'))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'search/static/'),
-    os.path.join(BASE_DIR, 'web/static/'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'search/static/'),
+#     os.path.join(BASE_DIR, 'web/static/'),
+# ]
